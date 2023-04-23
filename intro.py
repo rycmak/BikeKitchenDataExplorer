adc97dbcb6373c1ac2a1ace1826feba09826bec9
Rachel Mak <ryc.mak@gmail.com>
2023-04-24 01:17:51 +1200

    Changed font in intro

--- intro.py
+++ intro.py
@@ -2,11 +2,18 @@ import streamlit as st
 
 def app(data):
   st.image("bike_kitchen_logo.jpg", use_column_width=True)
-  st.title("Bike Kitchen Data")
+  st.subheader("Bike Kitchen Data")
     
-  st.subheader("Bike Kitchen is a non-profit focused on fixing and giving bikes \
-                 to those in our community who need one.")
-                 
-  st.subheader("Please use the navigation on the left to explore our data.")
+  st.markdown('<p style="font-family:sans-serif; font-size: 20px;">\
+                Bike Kitchen is a non-profit focused on fixing and giving bikes \
+                to those in our community who need one.\
+              </p>', unsafe_allow_html=True)
 
-  st.subheader("Happy exploring!")
+  st.markdown('<p style="font-family:sans-serif; font-size: 20px;">\
+                Please use the navigation on the left to explore our data.\
+              </p>', unsafe_allow_html=True)
+
+  st.markdown('<p style="font-family:sans-serif; font-size: 20px;">\
+                Happy exploring!\
+              </p>', unsafe_allow_html=True)
+                 
